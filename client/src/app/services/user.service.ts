ec1b38a753f7653b57d1e021b2a1eb86165df6c0
StefanAdyy <stefanmorariu@rocketmail.com>
2023-10-24 20:46:40 +0300

    Implemented login and logout

---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../shared/models/User';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { HttpClient } from '@angular/common/http';
+import { USER_LOGIN_URL } from '../shared/constants/urls';
+import { ToastrService } from 'ngx-toastr';
+
+const USER_KEY = 'User';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userSubject = new BehaviorSubject<User>(this.getUSerFromLocalStorage());
+  public userObservable: Observable<User>;
+
+  constructor(private http: HttpClient, private toastrService: ToastrService) {
+    this.userObservable = this.userSubject.asObservable();
+  }
+
+  login(userLogin: IUserLogin): Observable<User> {
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {
+          this.setUserToLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastrService.success(
+            `Bine ai venit la noi, ${user.name}!`,
+            'Login realizat cu succes!'
+          )
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Nu te-ai putut conecta!');
+        }
+      })
+    );
+  }
+
+  logout() {
+    this.userSubject.next(new User());
+    localStorage.removeItem(USER_KEY);
+    window.location.reload(); // facem reload ca atunci cand esti pe o pagina unde e necesar un cont (Cos) sa nu ramana acolo 
+  }
+
+  private setUserToLocalStorage(user: User) {
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  private getUSerFromLocalStorage(): User {
+    const userJson = localStorage.getItem(USER_KEY);
+
+    if (userJson) {
+      return JSON.parse(userJson) as User;
+    }
+
+    return new User();
+  }
+}
