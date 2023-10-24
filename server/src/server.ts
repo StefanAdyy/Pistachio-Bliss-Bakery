2cd881468162af2617ac16ad2e42402b28fb256f
StefanAdyy <stefanmorariu@rocketmail.com>
2023-10-24 18:53:26 +0300

    Added login api and jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express, { response } from "express";
 import cors from "cors";
-import { sample_foods, sample_tags } from "./data";
+import { sample_foods, sample_tags, sample_users } from "./data";
+import jwt from "jsonwebtoken";
 
 const app = express();
+app.use(express.json());
+
 app.use(cors({
     credentials: true,
     origin: ["http://localhost:4200"]
@@ -38,7 +41,31 @@ app.get("/api/foods/:foodId", (req, res) => {
     const food = sample_foods.find(food => food.id == foodId);
 
     res.send(food);
-})
+});
+
+app.post("/api/users/login", (req, res) => {
+    const { email, password } = req.body; //destructurin assignment
+    const user = sample_users.find(user => user.email === email &&
+        user.password === password);
+
+    if (user) {
+        res.send(generateTokenResponse(user));
+    }
+    else {
+        res.status(400).send("Adresa de e-mail sau parola sunt invalide!");
+    }
+});
+
+const generateTokenResponse = (user: any) => {
+    const token = jwt.sign({
+        email: user.email, isAdmin: user.isAdmin
+    }, "Text random", {
+        expiresIn: "30d"
+    });
+
+    user.token = token;
+    return user;
+}
 
 const port = 5000;
 
