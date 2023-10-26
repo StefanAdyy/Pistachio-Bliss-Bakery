860275d5b0aabfb1813bcabaf446b3ef5b061efd
StefanAdyy <stefanmorariu@rocketmail.com>
2023-10-26 08:29:49 +0300

    Updated button css

--- client/src/app/components/partials/default-button/default-button.component.ts
+++ client/src/app/components/partials/default-button/default-button.component.ts
@@ -11,7 +11,7 @@ export class DefaultButtonComponent {
   @Input()
   text: string = 'Continua';
   @Input()
-  bgColor = '#e72929';
+  bgColor = '#ADC247';
   @Input()
   color = 'white';
   @Input()
