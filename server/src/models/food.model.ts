524da50961a83d6303c79eb5b3f1401119b22e48
StefanAdyy <stefanmorariu@rocketmail.com>
2023-10-26 14:07:10 +0300

    Created food model

---
+++ server/src/models/food.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+export interface Food {
+    id: string;
+    name: string;
+    price: number;
+    tags: string[];
+    favorite: boolean;
+    imageUrl: string;
+    origins: string[];
+    preparationTime: string;
+}
+
+export const FoodSchema = new Schema<Food>(
+    {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        tags: { type: [String] },
+        favorite: { type: Boolean, default: false },
+        imageUrl: { type: String, required: true },
+        origins: { type: [String], required: true },
+        preparationTime: { type: String, required: true },
+    }, {
+    toJSON: {
+        virtuals: true
+    },
+    toObject: {
+        virtuals: true
+    },
+    timestamps: true
+}
+);
+
+export const FoodModel = model<Food>('food', FoodSchema);
