7fd7b0abd4206d238d7c382f0be1c078bdf21b33
StefanAdyy <stefanmorariu@rocketmail.com>
2023-11-06 17:14:37 +0200

    Added register service, url, interface for user

--- server/src/routers/user.router.ts
+++ server/src/routers/user.router.ts
@@ -2,7 +2,9 @@ import { Router } from 'express';
 import { sample_users } from '../data';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
-import { UserModel } from '../models/user.model';
+import { User, UserModel } from '../models/user.model';
+import { HTTP_BAD_REQUEST } from '../constants/http_status';
+import bcrypt from 'bcryptjs';
 
 const router = Router();
 
@@ -29,10 +31,38 @@ router.post("/login", asyncHandler(
             res.send(generateTokenResponse(user));
         }
         else {
-            res.status(400).send("Adresa de e-mail sau parola sunt invalide!");
+            res.status(HTTP_BAD_REQUEST).send("Adresa de e-mail sau parola sunt invalide!");
         }
     }
 ));
+
+router.post('/register', asyncHandler(
+    async (req, res) => {
+        const { name, email, password, address } = req.body;
+        const user = await UserModel.findOne({ email });
+
+        if (user) {
+            res.status(HTTP_BAD_REQUEST)
+                .send('Utilizatorul exista deja, conecteaza-te!');
+            return;
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const newUser: User = {
+            id: '',
+            name,
+            email: email.toLoweCase(),
+            password: encryptedPassword,
+            address,
+            isAdmin: false
+        }
+
+        const dbUser = await UserModel.create(newUser);
+
+        res.send(generateTokenResponse(dbUser));
+    }
+))
 
 const generateTokenResponse = (user: any) => {
     const token = jwt.sign({
