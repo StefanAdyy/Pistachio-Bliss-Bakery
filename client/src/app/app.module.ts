f579c11215413f31f5330bb31661115cb39de0cf
StefanAdyy <stefanmorariu@rocketmail.com>
2023-11-28 07:05:29 +0200

    Added payment page component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 import { OrderItemsListComponent } from './components/partials/order-items-list/order-items-list.component';
 import { AuthInterceptor } from './auth/auth.interceptor';
+import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { AuthInterceptor } from './auth/auth.interceptor';
     RegisterPageComponent,
     LoadingComponent,
     CheckoutPageComponent,
-    OrderItemsListComponent
+    OrderItemsListComponent,
+    PaymentPageComponent
   ],
   imports: [
     BrowserModule,
